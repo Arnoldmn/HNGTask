809c1ef771499d7e70527e66bd01b5429aa62bc4
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-09-18 09:32:30 +0300

    initialize ui variables

--- app/src/main/java/mna/mumbere/hngtask/RegisterActivity.kt
+++ app/src/main/java/mna/mumbere/hngtask/RegisterActivity.kt
@@ -2,10 +2,17 @@ package mna.mumbere.hngtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
 
+    lateinit var btnRegister: Button
+    lateinit var etEmail: EditText
+    lateinit var etFname: EditText
+    lateinit var etLname: EditText
+    lateinit var etPassword: EditText
 
     lateinit var mAuth: FirebaseAuth
 
