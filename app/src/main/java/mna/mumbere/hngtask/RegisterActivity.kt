65173ba4a892afe055380b1997d27dd2757b232b
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-09-18 09:26:50 +0300

    Add firebase Auth

--- app/src/main/java/mna/mumbere/hngtask/RegisterActivity.kt
+++ app/src/main/java/mna/mumbere/hngtask/RegisterActivity.kt
@@ -2,8 +2,12 @@ package mna.mumbere.hngtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+
+    lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
