e28bcd3db7aa0f61c6c80a660551eabc9c64ee32
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-09-18 11:33:48 +0300

    Add onStart method

--- app/src/main/java/mna/mumbere/hngtask/MainActivity.kt
+++ app/src/main/java/mna/mumbere/hngtask/MainActivity.kt
@@ -1,12 +1,35 @@
 package mna.mumbere.hngtask
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser == null){
+
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
